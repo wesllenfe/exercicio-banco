f39010e57b0ec939afe061d374e1b969837003cf
Wesllen Felipe Langaro Raiser da Cruz <129122783+wesllenfe@users.noreply.github.com>
2024-03-11 21:00:18 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio-banco/ContaBancaria.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+
+namespace exercicio_banco {
+    internal class ContaBancaria {
+
+            public int Numero { get; private set; }
+            public string Titular { get; set; }
+            public double Saldo { get; private set; }
+
+             public ContaBancaria(int numero, string titular) {
+                 Numero = numero;
+                 Titular = titular;
+             }
+
+             public ContaBancaria(int numero, string titular, double depositoInicial) : this(numero, titular) {
+                Deposito(depositoInicial);
+             }
+
+             public void Deposito(double quantia) {
+                Saldo += quantia;
+            }
+            
+            public void Saque(double quantia) {
+                Saldo -= quantia + 5;
+            }
+
+        public override string ToString() {
+            return "Conta " + Numero + 
+                ", Titular: " + Titular + 
+                ", Saldo: $" + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
